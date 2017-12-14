cb481edb0d1ae5f22f85d068bfa4c024bf9408f5
Pooja Suvarna <ps834@afsconnect2.njit.edu>
2017-12-14 15:41:39 -0500

    Convert the array in all tasks in a table format

--- mvc/pages/show_task.php
+++ mvc/pages/show_task.php
@@ -20,7 +20,7 @@
 <?php
 //this is how you print something  $data contains the record that was selected on the table.
 
-print_r($data);
+print utility\htmlTable::generateTableFromOneRecord($data);
 ?>
 <form action="index.php?page=tasks&action=delete&id=<?php echo $data->id; ?> " method="post" id="form1">
     <button type="submit" form="form1" value="delete">Delete</button>

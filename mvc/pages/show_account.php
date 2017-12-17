27aaf1e9a89ca697f6521fadecfed7939d52f6f8
Pooja Suvarna <ps834@afsconnect2.njit.edu>
2017-12-17 02:18:32 -0500

    Created Edit profile

--- mvc/pages/show_account.php
+++ mvc/pages/show_account.php
@@ -35,7 +35,7 @@ print utility\htmlTable::generateTableFromOneRecord($data);
 
 ?>
 
-
+<!--
 <form action="index.php?page=accounts&action=save&id=<?php echo $data->id; ?>" method="post">
 
     First name: <input type="text" name="fname" value="<?php echo $data->fname; ?>"><br>
@@ -50,7 +50,7 @@ print utility\htmlTable::generateTableFromOneRecord($data);
 
 <form action="index.php?page=accounts&action=delete&id=<?php echo $data->id; ?> " method="post" id="form1">
     <button type="submit" form="form1" value="delete">Delete</button>
-</form> 
+</form> -->
 
 
 <script src="js/scripts.js"></script>

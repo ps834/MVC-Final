fe1f51d5ec52f95b5d387a1f89d7b359a36117f5
Pooja Suvarna <ps834@afsconnect2.njit.edu>
2017-12-17 07:49:23 -0500

    Login Page

--- mvc/pages/homepage.php
+++ mvc/pages/homepage.php
@@ -13,16 +13,44 @@
     <!--[if lt IE 9]>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
     <![endif]-->
+    
+    
+     <style>
+  .modal-header, h4, .close {
+      background-color: #5cb85c;
+      color:white !important;
+      font-size: 30px;
+      width:40%;
+  }
+  .modal-footer {
+      background-color: #f9f9f9;
+  }
+  
+body {
+    font-family: 'Open Sans', sans-serif;
+  }
+  
+  
+button {
+
+      width:85px; 
+      height:50px;
+      padding-top:8px;
+      padding-left:12px;
+
+}
+
+.div-within {
+      
+      background-color:red;
+      width:40%;
+      height:400px;
+}
+  </style>
 </head>
 
 <body>
 
-
-<h1>
-    <?php
-    //this how to print some data;
-    echo $data['site_name'];
-    ?> </h1>
 
 <h3><a href="index.php?page=accounts&action=register" style="text-decoration:none;">Sign up</a></h3>
 
@@ -30,18 +58,29 @@
 <form action="index.php?page=accounts&action=login" method="POST">
 
     <div class="container" align="center">
-        <h3> Log In </h3><br>
-        <label><b>Username</b></label>
-        <input type="text" placeholder="Enter Username" name="email" required>
+    <div class="modal-header"> <h4><?php echo $data['site_name'];?></h4></div> 
+    <div class="div-within">
         <br><br>
-        <label><b>Password</b></label>&nbsp;
-        <input type="password" placeholder="Enter Password" name="password" required>
+        <label style="padding-right:126px;"><b>Username</b></label>
+        <input type="text" placeholder="Enter Username" name="email" required class="form-control" style="width:200px; height:40px;">
         <br><br>
-        <button type="submit">Login</button>
+        <label style="padding-right:126px;"><b>Password</b></label>&nbsp;
+        <input type="password" placeholder="Enter Password" name="password" required class="form-control" style="width:200px; height:40px;">
+        <br><br>
+        <button type="submit" class="btn btn-info active" >Login</button>
+    <div>
     </div>
 
 
 </form>
+
+
+
+ 
+
+ 
+
+
 
 
 <script src="js/scripts.js"></script>
